c815c5e02e30808706a01ba5825d0bda2fb42f7d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-11-14 13:09:48 +0100

    cd..

--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -1,6 +1,7 @@
 class Student(name: String = "Przemysław", surname: String = "Stokłosa"){
     var array: Array<Int>
     var weight: Double = 0.0
+    var myNo: Int = 0
 
     constructor(weight: Double):this("Przemysław","Stokłosa"){
         this.weight = weight
@@ -9,5 +10,15 @@ class Student(name: String = "Przemysław", surname: String = "Stokłosa"){
     init{
         array = arrayOf(1,2,3,4,5)
         println("init...")
+        myNo = noStudents+1
+        noStudents++
+    }
+
+    companion object{
+        var noStudents = 0
+
+        fun studentsInfo(){
+            println("Liczba studentów w bazie: $noStudents")
+        }
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,4 +2,6 @@ fun main() {
     val student = Student("Przemysław","Stokłosa")
     val student1 = Student(10.0)
     val student2 = Student()
+
+    Student.studentsInfo()
 }
